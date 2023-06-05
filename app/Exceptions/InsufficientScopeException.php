7835dfe6c0795351b2cfa567eaea16ff3b75a8d1
john ore <jore@g2o.com>
2023-06-05 10:40:35 -0400

    0Auth permissions

---
+++ app/Exceptions/InsufficientScopeException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\Response;
+
+class InsufficientScopeException extends Exception
+{
+
+  public function __construct()
+  {
+
+    parent::__construct('Insufficient scope', Response::HTTP_FORBIDDEN);
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,26 @@
 |
 */
 
-$router->group(['prefix' => 'api'], function () use ($router) {
+$router->group(['prefix' => 'api', 'middleware' => 'auth'], function () use ($router) {
   $router->get('resources',  ['uses' => 'ResourcesController@showAllResources']);
   $router->get('resources/{id}', ['uses' => 'ResourcesController@showOneResource']);
-  $router->post('resources', ['uses' => 'ResourcesController@create']);
   $router->put('resources/{id}', ['uses' => 'ResourcesController@update']);
   $router->delete('resources/{id}', ['uses' => 'ResourcesController@delete']);
 
   $router->get('users',  ['uses' => 'UserController@showAllUsers']);
   $router->get('users/{id}', ['uses' => 'UserController@showOneUser']);
-  $router->post('users', ['uses' => 'UserController@create']);
   $router->put('users/{id}', ['uses' => 'UserController@update']);
   $router->delete('users/{id}', ['uses' => 'UserController@delete']);
 
   // Add routes for creating, updating, and deleting resources as needed
 });
+
+$router->post('api/resources', [
+  'middleware' => 'auth:create:resource',
+  'uses' => 'ResourcesController@create'
+]);
+
+$router->post('api/users', [
+  'middleware' => 'auth:create:user',
+  'uses' => 'UserController@create'
+]);
